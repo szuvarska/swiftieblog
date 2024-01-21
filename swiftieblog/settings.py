5a18a2e12cc9bfd207d7ffe71294d92589aabb9b
szuvarska <01171269@pw.edu.pl>
2024-01-21 23:58:12 +0100

    Add docker

--- swiftieblog/settings.py
+++ swiftieblog/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'swiftieblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
